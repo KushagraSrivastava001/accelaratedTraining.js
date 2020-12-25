d3f648957cf8c1bc89cc50de6a866bf4c1d2dc97
Kushagra <63976266+CodeGodZeus@users.noreply.github.com>
2020-12-25 13:40:47 +0530

    Create loops.js

---
+++ loops.js
@@ -0,0 +1,35 @@
+
+//do while
+var conditions=false
+do
+{
+console.log("code executed")
+}
+while(conditions)
+
+
+
+//while
+flag=false
+while(flag)
+{
+console.log("executed")
+}
+
+//switch
+var key=8
+
+switch(key):
+case 1:
+console.log("case1")
+break
+case 2:
+console.log("case 2")
+break;
+:::::::
+:::::::
+:::::::
+:::::::
+default:
+console.log("kushagra_is_gr8")
+
